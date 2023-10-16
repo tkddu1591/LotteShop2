792f34d047979e0710db2cfadb50b15a753ad07d
mintchoc31 <mintchoc31@gmail.com>
2023-10-16 17:12:14 +0900

    admin product  기능구현 중

---
+++ src/main/java/com/example/kmarket/controller/admin/product/RegisterController.java
@@ -0,0 +1,16 @@
+package com.example.kmarket.controller.admin.product;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Log4j2
+@Controller
+public class RegisterController {
+
+  /*GetMapping("/admin/product/register")
+    public string register(){
+    }
+ /
+
+}
