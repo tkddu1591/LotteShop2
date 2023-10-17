c2217aaaa4870a4fbc4efcc9db56f45199ed3766
김상엽_노트북 <tkddu1591@gamil,com>
2023-10-17 13:09:14 +0900

    WebConfig 설정 추가

---
+++ src/main/java/com/example/kmarket/security/WebConfig.java
@@ -0,0 +1,20 @@
+package com.example.kmarket.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableScheduling
+@EnableTransactionManagement
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("**")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
+    }
+}
