fa766d0c78425ec9030ce8ffc3f868c87d7586bb
mintchoc31 <mintchoc31@gmail.com>
2023-10-24 13:53:07 +0900

    10.24 admin-product-register 기능 구현중

--- src/main/java/com/example/kmarket/controller/admin/product/RegisterController.java
+++ src/main/java/com/example/kmarket/controller/admin/product/RegisterController.java
@@ -2,11 +2,9 @@ package com.example.kmarket.controller.admin.product;
 
 import com.example.kmarket.dto.product.KmProductDTO;
 import com.example.kmarket.service.admin.KmAdminRegisterService;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -26,23 +24,10 @@ public class RegisterController {
 
     @PostMapping("/admin/product/register")
     public String register(KmProductDTO dto){
-     /*
-      String prodName
-      String descript
-      String company
-      int price
-      int discount
-      int stock
-      int delivery
-      String status
-      String duty
-      String receipt
-      String bizType
-      String origin
 
+      kmAdminRegisterService.insertProduct(dto);
 
-      */
-      return null;
+      return "redirect:/admin/product/register";
 
     }
 
