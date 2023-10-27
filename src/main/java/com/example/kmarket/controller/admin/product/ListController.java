f64509d243061f13c0c997a7734de32c50306a6a
mintchoc31 <mintchoc31@gmail.com>
2023-10-27 17:37:34 +0900

    10.27 admin-product-list 상품 목록 검색 기능 구현중

--- src/main/java/com/example/kmarket/controller/admin/product/ListController.java
+++ src/main/java/com/example/kmarket/controller/admin/product/ListController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -41,6 +42,7 @@ public class ListController {
 
         return "/admin/product/list";
     }
+
 
 
 
